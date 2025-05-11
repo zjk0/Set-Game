4fb341441d76abd071619de8f25817d6a0c26f76
zjk0 <2054295479@qq.com>
2025-05-11 17:37:38 +0800

    finish version-2

--- SetGame.py
+++ SetGame.py
@@ -37,34 +37,85 @@ def show_trans_image (image_array):
 
 '''
 @brief 获取纸牌位置和大小
-@param img_bin: 二值化后的含有纸牌的图像
+@param img_preproc: 预处理后的图像
 @return cards_info: 纸牌信息, 与图像中的纸牌一一对应, 元素的形式为[行数, 列数, 宽, 高]
 '''
-def get_cards_info (img_bin):
+def get_cards_info (img_preproc):
+    contours, _ = cv.findContours(img_preproc, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)  # 检测出连通域的边缘
+    cards_info_list = []  # 纸牌信息列表
+
     # 获取纸牌信息
-    contours, _ = cv.findContours(img_bin, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)  # 检测出连通域的边缘
-    cards_info_list = []  # 纸牌中心位置列表
     for contour in contours:
+        # 矩形逼近, 得到矩形信息
         rect = cv.minAreaRect(contour)
 
         # 根据宽和高进行二次筛选
         if rect[1][0] > 100 and rect[1][1] > 100:
-            # 整理成(row, column, w, h)的形式, 其中 w < h
-            if rect[1][0] < rect[1][1]:  # (w, h)的形式, 较短的数值被认为是w
-                card_info = (rect[0][1], rect[0][0], rect[1][0], rect[1][1])
-            else:
-                card_info = (rect[0][1], rect[0][0], rect[1][1], rect[1][0])
-            cards_info_list.append(card_info)
-            
+            if rect[1][0] > 200 or rect[1][1] > 200:  # 两张牌距离太近, 被识别成同一连通域
+                if rect[1][0] > 200 and rect[1][1] < 200:
+                    # 两张纸牌相连方向上的长度除以2, 得到正常情况下的数值
+                    value = int(rect[1][0] / 2)
+
+                    # 较短的数值被认为是w
+                    if value < rect[1][1]:
+                        w = value
+                        h = rect[1][1]
+
+                        # 确定两张牌的中心
+                        center1 = (rect[0][1], rect[0][0] - value / 2)
+                        center2 = (rect[0][1], rect[0][0] + value / 2)
+                    else:
+                        w = rect[1][1]
+                        h = value
+
+                        # 确定两张牌的中心
+                        center1 = (rect[0][1] - value / 2, rect[0][0])
+                        center2 = (rect[0][1] + value / 2, rect[0][0])
+                elif rect[1][0] < 200 and rect[1][1] > 200:
+                    # 两张纸牌相连方向上的长度除以2, 得到正常情况下的数值
+                    value = int(rect[1][1] / 2)
+
+                    # 较短的数值被认为是w
+                    if value < rect[1][0]:
+                        w = value
+                        h = rect[1][0]
+
+                        # 确定两张牌的中心
+                        center1 = (rect[0][1], rect[0][0] - value / 2)
+                        center2 = (rect[0][1], rect[0][0] + value / 2)
+                    else:
+                        w = rect[1][0]
+                        h = value
+
+                        # 确定两张牌的中心
+                        center1 = (rect[0][1] - value / 2, rect[0][0])
+                        center2 = (rect[0][1] + value / 2, rect[0][0])
+
+                # 添加至列表
+                card_info1 = (center1[0], center1[1], w, h)
+                card_info2 = (center2[0], center2[1], w, h)
+                cards_info_list.append(card_info1)
+                cards_info_list.append(card_info2)
+            elif rect[1][0] < 200 and rect[1][1] < 200:  # 距离正常
+                # 较短的数值被认为是w
+                if rect[1][0] < rect[1][1]:
+                    card_info = (rect[0][1], rect[0][0], rect[1][0], rect[1][1])
+                else:
+                    card_info = (rect[0][1], rect[0][0], rect[1][1], rect[1][0])
+                cards_info_list.append(card_info)
+    
     # 得到纸牌位置数组, 并进行形状重塑和元素位置调整, 调整成能与图像中纸牌一一对应的形式
     cards_info = np.array(cards_info_list)  # 转换为numpy数组
     cards_info = cards_info.astype(int)
+    sort_index = np.argsort(cards_info[:, 0])  # 对行坐标进行排序
+    cards_info = cards_info[sort_index]  # 根据行坐标的排序结果重塑数组
     cards_info = cards_info.reshape(3, 4, 4)
-    cards_info = cards_info[::-1, :, :]
     sort_index = np.argsort(cards_info[:, :, 1])  # 对每个坐标的列坐标进行排序, 得到排序后的元素在排序前数组中的索引
     row_index = np.arange(cards_info.shape[0])[:, None]
     cards_info = cards_info[row_index, sort_index, :]
 
+    # print("cards_info:")
+    # print(cards_info)
     return cards_info
 
 '''
@@ -78,28 +129,28 @@ def get_color (img_rgb, cards_info):
     cards_info_ = cards_info.reshape(-1, 4)
 
     for card_info in cards_info_:
-        # 确定检测的区域, 加减10是为了防止取到纸牌之外的区域
-        center = (card_info[0], card_info[1])
-        up = center[0] - int(card_info[3] / 2) + 10
-        down = center[0] + int(card_info[3] / 2) - 10
-        analysis_array = img_rgb[up : down, center[1], :]
+        # 获取感兴趣区域, 加减10是为了防止取到纸牌之外的区域
+        row_min = card_info[0] - int(card_info[3] / 2) + 10
+        row_max = card_info[0] + int(card_info[3] / 2) - 10
+        column_min = card_info[1] - int(card_info[2] / 2) + 10
+        column_max = card_info[1] + int(card_info[2] / 2) - 10
+        roi = img_rgb[row_min : row_max + 1, column_min : column_max + 1, :]
 
         # 转换为hsv空间
-        analysis_array = np.expand_dims(analysis_array, axis = 1)
-        hsv_array = ImgProc.rgb_to_hsv(analysis_array)
+        roi_hsv = ImgProc.rgb_to_hsv(roi) 
 
-        # 颜色检测
+        # 遍历经过感兴趣区域中心点的竖直直线来进行颜色检测
         index = np.where(np.all(cards_info == card_info, axis = -1))
-        for i in range(hsv_array.shape[0]):
+        for i in range(roi_hsv.shape[0]):
             # 获取hsv三个值
-            h = hsv_array[i, 0, 0]
-            s = hsv_array[i, 0, 1]
-            v = hsv_array[i, 0, 2]
+            h = roi_hsv[i, int(roi_hsv.shape[1] / 2), 0]
+            s = roi_hsv[i, int(roi_hsv.shape[1] / 2), 1]
+            v = roi_hsv[i, int(roi_hsv.shape[1] / 2), 2]
 
             # 判断颜色
             is_red = ((h >= 0 and h <= 10) or (h >= 245 and h <= 255)) and s >= 50 and v >= 50
             is_green = (h >= 64 and h <= 106) and s >= 50 and v >= 50
-            is_purple = (h >= 180 and h <= 230) and s >= 10 and (v >= 10 and v <= 200)
+            is_purple = (h >= 180 and h <= 230) and s >= 25 and (v >= 10 and v <= 200)
 
             # 对颜色矩阵赋值
             if is_red:
@@ -118,20 +169,15 @@ def get_color (img_rgb, cards_info):
 
 '''
 @brief 获取纸牌中的图形形状
-@param img_bin: 二值化后的含有纸牌的图像
+@param img_gray: 灰度图像
 @param cards_info: 纸牌信息(位置和大小)
 @return appearance_matrix: 形状矩阵, 与图像中的纸牌一一对应, 1: 菱形, 2: 椭圆, 3: 波浪
 '''
-def get_appearance (img_bin, cards_info):
-    # 开运算, 消除条纹, 使得后续边缘检测不受条纹影响
-    img_bin = ImgProc.morphology_process(img_bin, method = "opening", dilation_se_size = 7, erosion_se_size = 9)
-
+def get_appearance (img_gray, cards_info):
     # 形状识别
     cards_info_ = cards_info.reshape(-1, 4)
     appearance_matrix = np.zeros((3, 4), dtype = int)
-    pos = 0
     for card_info in cards_info_:
-        pos += 1
         index = np.where(np.all(cards_info == card_info, axis = -1))
 
         # 获取感兴趣区域, 加减10是为了防止取到纸牌之外的区域
@@ -139,90 +185,78 @@ def get_appearance (img_bin, cards_info):
         row_max = card_info[0] + int(card_info[3] / 2) - 10
         column_min = card_info[1] - int(card_info[2] / 2) + 10
         column_max = card_info[1] + int(card_info[2] / 2) - 10
-        roi = img_bin[row_min : row_max + 1, column_min : column_max + 1]
-        
-        # 对感兴趣区域进行处理, 便于进行连通域检测
-        roi = roi.astype(np.uint8)  # 转换为适合取反的类型
-        roi = ~roi  # 取反, 方便进行连通域检测
-        roi = ImgProc.morphology_process(roi, method = "opening", dilation_se_size = 3, erosion_se_size = 3)  # 开运算, 消去一些噪点
-        roi = roi.astype(np.uint8)  # 转换为uint8类型
+        roi = img_gray[row_min : row_max + 1, column_min : column_max + 1]
 
-        # 直线检测, 通过检测到的直线数量, 进行第一次筛选
-        roi_edge = cv.Canny(roi, 180, 240)  # Canny边缘检测
-        lines = cv.HoughLinesP(roi_edge, rho = 1, theta = np.pi / 180, threshold = 30, minLineLength = 30, maxLineGap = 5)  # 霍夫变换检测直线
-        if lines is None:  # 获取直线数量
-            lines_num = 0
-        else:
-            lines_num = lines.shape[0]
-
-        if lines_num == 0:  # 如果检测到的直线的数量等于0, 说明图形形状为波浪
-            appearance_matrix[index] = 3
-            continue  # 如果已经确定是波浪, 就不用进行第二次筛选
+        # 对感兴趣区域进行预处理
+        roi_edge = cv.Canny(roi, 100, 200)
+        roi_preproc = ImgProc.morphology_process(roi_edge, method = "closing", dilation_se_size = 9, erosion_se_size = 7)
+        roi_preproc = roi_preproc.astype(np.uint8)
         
-        # 通过计算圆度来进行第二次筛选
-        contours, _ = cv.findContours(roi, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)  # 连通域检测, 得到连通域边缘
+        # 通过计算圆度来进行第一次筛选
+        contours, _ = cv.findContours(roi_preproc, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)  # 连通域检测, 得到连通域边缘
         c_list = []  # 圆度列表
-        for i in range(len(contours)):
-            # print(len(contours[i]))
-            if len(contours[i]) > 50:  # 选取一条足够长的轮廓, 防止取到一些噪声导致的轮廓
-                cnt = contours[i]
-                area = cv.contourArea(cnt)  # 连通域面积
-                length = cv.arcLength(cnt, True)  # 连通域轮廓周长
+        for contour in contours:
+            if len(contour) > 50:  # 选取一条足够长的轮廓, 防止取到一些噪声导致的轮廓
+                area = cv.contourArea(contour)  # 连通域面积
+                length = cv.arcLength(contour, True)  # 连通域轮廓周长
                 c = (4 * np.pi * area) / (length ** 2)  # 圆度
                 c_list.append(c)  # 添加至列表最后
 
         c_array = np.array(c_list)
         c_mean = c_array.mean()  # 计算圆度平均值
-        if c_mean > 0.65:  # 如果圆度平均值较大, 则认为是椭圆
+        if c_mean > 0.7:  # 如果圆度平均值较大, 则认为是椭圆
             appearance_matrix[index] = 2
-        else:  # 如果圆度平均值较小, 则认为是菱形
-            appearance_matrix[index] = 1
+            continue  # 如果已经确定是椭圆, 就不用进行第二次筛选
 
-    # print("appearance:")
-    # print(appearance_matrix)
+        # 直线检测, 通过检测到的直线数量, 进行第二次筛选
+        roi_edge = cv.Canny(roi_preproc, 180, 240)  # Canny边缘检测
+        lines = cv.HoughLinesP(roi_edge, rho = 1, theta = np.pi / 180, threshold = 30, minLineLength = 25, maxLineGap = 5)  # 霍夫变换检测直线
+        if lines is None:  # 获取直线数量
+            lines_num = 0
+        else:
+            lines_num = lines.shape[0]
+
+        if lines_num < 3:
+            appearance_matrix[index] = 3  # 如果检测到的直线的数量较少, 说明图形形状为波浪
+        else:
+            appearance_matrix[index] = 1  # 如果检测到的直线的数量较多, 说明图形形状为菱形
+
+    print("appearance:")
+    print(appearance_matrix)
     return appearance_matrix
-    
 
 '''
 @brief 获取纸牌中的图形的数量
-@param img_bin: 二值化后的含有纸牌的图像
+@param img_gray: 灰度图像
 @param cards_info: 纸牌信息(位置和大小)
 @return number_matrix: 个数矩阵, 与图像中的纸牌一一对应
 '''
-def get_number (img_bin, cards_info):
-    # 开运算, 消除条纹, 使条纹变为实心, 此时有实心和空心两种纹路
-    img_bin = ImgProc.morphology_process(img_bin, method = "opening", dilation_se_size = 7, erosion_se_size = 9)
-
+def get_number (img_gray, cards_info):
     # 获取图形个数
     number_matrix = np.zeros((3, 4), dtype = int)
     cards_info_ = cards_info.reshape(-1, 4)
     for card_info in cards_info_:
-        count = 0  # 记录数值变化的次数
-        first_to_second = 0  # 记录第一次变化到第二次变化的距离
-        center = (card_info[0], card_info[1])
+        # 获取感兴趣区域, 加减10是为了防止取到纸牌之外的区域
+        row_min = card_info[0] - int(card_info[3] / 2) + 10
+        row_max = card_info[0] + int(card_info[3] / 2) - 10
+        column_min = card_info[1] - int(card_info[2] / 2) + 10
+        column_max = card_info[1] + int(card_info[2] / 2) - 10
+        roi = img_gray[row_min : row_max + 1, column_min : column_max + 1]
 
-        # 从中心竖直向下遍历, 图形与纸牌边界有一定距离, 减10可以使得在遍历时, 不会遍历到纸牌外的区域
-        for i in range(center[0], (center[0] + int(card_info[3] / 2) - 10)):
-            if i != center[0]:
-                if img_bin[i, center[1]] != img_bin[i - 1, center[1]]:  # 数值变化, 次数加1
-                    count += 1
-                    
-                if count == 1:
-                    first_to_second += 1
+        # 对感兴趣区域进行预处理
+        roi_edge = cv.Canny(roi, 100, 200)
+        roi_preproc = ImgProc.morphology_process(roi_edge, method = "closing", dilation_se_size = 9, erosion_se_size = 7)
+        roi_preproc = roi_preproc.astype(np.uint8)
 
-        # 判断个数
+        # 检测个数
+        contours, _ = cv.findContours(roi_preproc, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)  # 连通域检测, 得到连通域边缘
+        count = 0
+        for contour in contours:
+            if len(contour) > 40:  # 选取一条足够长的轮廓, 防止取到一些噪声导致的轮廓
+                count += 1
+
         index = np.where(np.all(cards_info == card_info, axis = -1))
-        if count == 3 or count == 6:  # 次数为3（空心纹路）或6（实心纹路）
-            number_matrix[index] = 3
-        elif count == 4:  # 次数为4（实心纹路）
-            number_matrix[index] = 2
-        elif count == 1:  # 次数为1（空心纹路）
-            number_matrix[index] = 1
-        elif count == 2:  # 次数为2
-            if first_to_second > 10:  # 距离较长（实心纹路）
-                number_matrix[index] = 2
-            else:  # 距离较短（空心纹路）
-                number_matrix[index] = 1
+        number_matrix[index] = count
 
     # print("number:")
     # print(number_matrix)
@@ -230,37 +264,60 @@ def get_number (img_bin, cards_info):
     
 '''
 @brief 获取图形中的纹路
-@param img_bin: 二值化后的含有纸牌的图像
+@param img_gray: 灰度图像
 @param cards_info: 纸牌信息(位置和大小)
 @param number: 图形的个数矩阵
 @return teture_matrix: 纹路矩阵, 与图像中的纸牌一一对应, 1: 空心, 2: 实心, 3: 条纹
 '''
-def get_texture (img_bin, cards_info, number):
+def get_texture (img_gray, cards_info, number):
     cards_info_ = cards_info.reshape(-1, 4)
     near_size = 10
     texture_matrix = np.zeros((3, 4), dtype = int)
+    pos = 0
     for card_info in cards_info_:
-        center = (card_info[0], card_info[1])
+        pos += 1
+        # 获取感兴趣区域, 加减10是为了防止取到纸牌之外的区域
+        row_min = card_info[0] - int(card_info[3] / 2) + 10
+        row_max = card_info[0] + int(card_info[3] / 2) - 10
+        column_min = card_info[1] - int(card_info[2] / 2) + 10
+        column_max = card_info[1] + int(card_info[2] / 2) - 10
+        roi = img_gray[row_min : row_max + 1, column_min : column_max + 1]
 
+        # 对感兴趣区域进行预处理, 这里进行的是Canny边缘检测得到边缘
+        roi_edge = cv.Canny(roi, 50, 150)
+
+        # 第一次筛选
+        center = (int(roi.shape[0] / 2), int(roi.shape[1] / 2))  # 获取感兴趣区域的中心
         index = np.where(np.all(cards_info == card_info, axis = -1))
         if number[index] == 1 or number[index] == 3:
             # 取以中心点为中心的一条直线
-            near_area = img_bin[center[0], center[1] - near_size : center[1] + near_size + 1]
+            near_area = roi_edge[center[0], center[1] - near_size : center[1] + near_size + 1]
         elif number[index] == 2:
             # 分别向上和向下取直线, 然后合并
-            near_area_1 = img_bin[center[0] + 20, center[1] - near_size : center[1] + near_size + 1]
-            near_area_2 = img_bin[center[0] - 20, center[1] - near_size : center[1] + near_size + 1]
+            near_area_1 = roi_edge[center[0] + 20, center[1] - near_size : center[1] + near_size + 1]
+            near_area_2 = roi_edge[center[0] - 20, center[1] - near_size : center[1] + near_size + 1]
             near_area = np.concatenate((near_area_1, near_area_2))
 
         var = np.var(near_area)  # 计算方差
         if var > 0:  # 如果方差大于0, 图形内部像素有变化, 说明是条纹纹路
             texture_matrix[index] = 3
-        else:  # 如果方差等于0
-            mean = np.mean(near_area)  # 求均值
-            if mean < 127:  # 如果均值较小, 说明图形内部偏向于黑色, 说明是实心纹路
-                texture_matrix[index] = 2
-            else:  # 如果均值较大, 说明图形内部偏向于白色, 说明是空心纹路
-                texture_matrix[index] = 1
+            continue  # 如果确定是条纹纹路了, 就不进行第二次筛选了
+
+        # 第二次筛选
+        if number[index] == 1 or number[index] == 3:
+            # 取以中心点为中心的一条直线
+            near_area = roi[center[0], center[1] - near_size : center[1] + near_size + 1]
+        elif number[index] == 2:
+            # 分别向上和向下取直线, 然后合并
+            near_area_1 = roi[center[0] + 20, center[1] - near_size : center[1] + near_size + 1]
+            near_area_2 = roi[center[0] - 20, center[1] - near_size : center[1] + near_size + 1]
+            near_area = np.concatenate((near_area_1, near_area_2))
+
+        mean = np.mean(near_area)  # 计算均值
+        if mean > 150:  # 如果均值较大, 说明图形内部像素值较高, 说明纹路为空心
+            texture_matrix[index] = 1
+        else:  # 如果均值较大, 说明图形内部像素值较低, 说明纹路为实心
+            texture_matrix[index] = 2
 
     # print("texture:")
     # print(texture_matrix)
@@ -369,16 +426,33 @@ def show_all_set (all_set_pos, cards_info):
 
     show_trans_image(img_copy)
 
+'''
+@brief 图像预处理
+@param img_gray: 灰度图像
+@return img_preporc: 预处理后的图像
+'''
+def image_preprocess (img_gray, threshold):
+    img_preporc = img_gray.copy()
+    img_preporc[img_preporc > threshold] = 255
+    img_preporc[img_preporc != 255] = 0
+    return img_preporc
+
+'''
+@brief 寻找可以组成set的纸牌, 并标记出来
+'''
 def search_set ():
     global img_rgb_resize, img_gray_resize
 
-    # 获取特征
-    img_bin = ImgProc.image_binarization(img_gray_resize, threshold = 180)  # 图像二值化
-    cards_info = get_cards_info(img_bin)  # 获取纸牌信息
-    number = get_number(img_bin, cards_info)  # 获取图形个数
-    texture = get_texture(img_bin, cards_info, number)  # 获取纹路
+    # 预处理
+    img_preproc = image_preprocess(img_gray_resize, threshold = 125)
+    # show_trans_image(img_preproc)
+
+    # 获取纸牌特征
+    cards_info = get_cards_info(img_preproc)  # 获取纸牌信息
+    number = get_number(img_gray_resize, cards_info)  # 获取图形个数
+    texture = get_texture(img_gray_resize, cards_info, number)  # 获取纹路
     color = get_color(img_rgb_resize, cards_info)  # 获取颜色
-    appearance = get_appearance(img_bin, cards_info)  # 获取形状
+    appearance = get_appearance(img_gray_resize, cards_info)  # 获取形状
     cards_feature = np.stack([number, texture, color, appearance], axis = 2)  # 每一个元素的形式为[个数, 纹路, 颜色, 形状]
 
     # 获取所有set的位置
